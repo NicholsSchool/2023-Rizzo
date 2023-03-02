58a1e542109cedea09912b8cf9f3cb4e57448f2b
Anthony Honeine <anthonyhoneine@hotmail.com>
2023-03-02 17:59:54 -0500

    added ArmConstants and raiser

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -142,4 +142,8 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final class ArmConstants {
+    public static final int RAISER_ID = -1; // replace with real ID
+  }
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,11 +4,24 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import frc.robot.Constants.ArmConstants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
+  private WPI_TalonFX raiser;
   /** Creates a new Arm. */
-  public Arm() {}
+  public Arm() {
+    raiser = new WPI_TalonFX(ArmConstants.RAISER_ID);
+    raiser.configFactoryDefault();
+    raiser.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    raiser.setInverted(true); // check if change needed
+    raiser.setNeutralMode(NeutralMode.Coast);
+  }
 
   @Override
   public void periodic() {
