e6e25b75dfe8cf3f1ff071ed4ece545d7b4922ac
Jonathan R. Kiddy <jon.kiddy@gmail.com>
2023-03-03 17:01:57 -0500

    commented out import line that was causing issues

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.PID;
 
 import frc.robot.Constants.ArmConstants;
 
