08fabb562f6124e468bf33846c84d2b38ff6bbc2
Jonathan R. Kiddy <jon.kiddy@gmail.com>
2023-03-11 11:39:34 -0500

    moved default auto into a autonmous package at the root of the robot package

--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
